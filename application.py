e43c0a912079e47b33ad85c522ffc8c3484f89d0
samj1098 <153657814+samj1098@users.noreply.github.com>
2025-03-15 15:45:11 -0700

    Added portfolio pages and minimal styles

--- application.py
+++ application.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
 @application.route('/')
 def home():
-    return "<h1>Welcome to My New AWS Flask Website!</h1>"
+    return render_template("index.html")
+
+@application.route('/projects')
+def projects():
+    return render_template("projects.html")
+
+@application.route('/about')
+def about():
+    return render_template("about.html")
+
+@application.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=8080)
